2cc65c9dc3fe9dbf017c763b596a344b3c8dfdce
torvista <4391026+torvista@users.noreply.github.com>
2024-11-28 23:02:59 +0100

    merge file structure for easier compare, convert language files

---
+++ files/admin/includes/languages/english/lang.manufacturers.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin GPSR
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Manufacturers',
+    'TABLE_HEADING_MANUFACTURERS' => 'Manufacturers',
+    'TEXT_HEADING_NEW_MANUFACTURER' => 'New Manufacturer',
+    'TEXT_HEADING_EDIT_MANUFACTURER' => 'Edit Manufacturer',
+    'TEXT_HEADING_DELETE_MANUFACTURER' => 'Delete Manufacturer',
+    'TEXT_MANUFACTURERS' => 'Manufacturers:',
+    'TEXT_NEW_INTRO' => 'Please fill out the following information for the new manufacturer',
+    'TEXT_MANUFACTURERS_NAME' => 'Manufacturers Name:',
+    'TEXT_MANUFACTURERS_IMAGE' => 'Manufacturers Image:',
+    'TEXT_MANUFACTURERS_URL' => 'Manufacturers URL:',
+    'TEXT_DELETE_INTRO' => 'Are you sure you want to delete this manufacturer?',
+    'TEXT_DELETE_PRODUCTS' => 'Delete products from this manufacturer? (including product reviews, products on special, upcoming products)',
+    'TEXT_DELETE_WARNING_PRODUCTS' => '<b>WARNING:</b> There are %s products still linked to this manufacturer!',
+    'TABLE_HEADING_MANUFACTURER_FEATURED' => 'Featured?',
+    'TEXT_MANUFACTURER_FEATURED_LABEL' => 'Featured?',
+    'TEXT_MANUFACTURER_IS_FEATURED' => 'Featured',
+//GPSR additions	
+    'TEXT_MANUFACTURERS_GPSR_GENERAL' => '<b>GPSR Additional Info for Manufacturer:</b>',
+    'TEXT_MANUFACTURERS_GPSR_NONEU' => '<b>Additional Info about importer for manufacturers outside EU:</b>',
+    'TEXT_MANUFACTURERS_GPSR_ADDITIONAL' => '<b>Other optional texts:</b>',
+
+    'TEXT_MANUFACTURERS_GPSR_COMPANY' => 'Manufacturer Company Name:',
+    'TEXT_MANUFACTURERS_GPSR_COMPANY_NONEU' => 'Importer Company Name:',
+    'TEXT_MANUFACTURERS_GPSR_CONTACT_PERSON' => 'Manufacturer Contact Person:',
+    'TEXT_MANUFACTURERS_GPSR_CONTACT_PERSON_NONEU' => 'Importer Responsible Person:',
+    'TEXT_MANUFACTURERS_GPSR_STREET' => 'Street:',
+    'TEXT_MANUFACTURERS_GPSR_POSTCODE' => 'Postcode:',
+    'TEXT_MANUFACTURERS_GPSR_CITY' => 'City:',
+    'TEXT_MANUFACTURERS_GPSR_COUNTRY' => 'Country:',
+    'TEXT_MANUFACTURERS_GPSR_URL' => 'Contact Website:',
+    'TEXT_MANUFACTURERS_GPSR_ADDITIONAL_1' => 'Additional Info 1:',
+    'TEXT_MANUFACTURERS_GPSR_ADDITIONAL_2' => 'Additional Info 2:',
+    'TEXT_MANUFACTURERS_GPSR_ADDITIONAL_3' => 'Additional Info 3:',
+    'TABLE_HEADING_MANUFACTURER_COMPANY' => 'GPSR Manufacturer Company Name',
+    'TABLE_HEADING_IMPORT_COMPANY' => 'GPSR Importer Company Name',
+];
+
+return $define;
---
+++ files/includes/languages/english/extra_definitions/lang.manufacturers_gpsr.php
@@ -0,0 +1,36 @@
+<?php
+/** 
+ * Plugin GPSR
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * Zen Cart German Version - www.zen-cart-pro.at
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+ * @version $Id: lang.manufacturers_gpsr.php 2024-11-28 torvista $
+ */
+
+$define = [
+    'TEXT_MANUFACTURER_GPSR_INFO' => 'Manufacturer Info ',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO' => 'Contact information in accordance with Art. 19 EU GPSR',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO_NONEU' => 'Person responsible in accordance with Art. 19 EU GPSR',
+    'TEXT_MANUFACTURER_GPSR_ADDRESS_INFO' => 'Post Address:',
+    'TEXT_MANUFACTURER_GPSR_URL_INFO' => 'Electronical Address:',
+    'TEXT_MANUFACTURER_GPSR_COMPANY' => '',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_PERSON' => '',
+    'TEXT_MANUFACTURER_GPSR_STREET' => '',
+    'TEXT_MANUFACTURER_GPSR_POSTCODE' => '',
+    'TEXT_MANUFACTURER_GPSR_CITY' => '',
+    'TEXT_MANUFACTURER_GPSR_COUNTRY' => '',
+    'TEXT_MANUFACTURER_GPSR_URL' => 'Website: ',
+    'TEXT_MANUFACTURER_GPSR_COMPANY_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_PERSON_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_STREET_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_POSTCODE_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_CITY_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_COUNTRY_NONEU' => '',
+    'TEXT_MANUFACTURER_GPSR_URL_NONEU' => 'Website: ',
+    'TEXT_MANUFACTURER_GPSR_ADDITIONAL_1' => '',
+    'TEXT_MANUFACTURER_GPSR_ADDITIONAL_2' => '',
+    'TEXT_MANUFACTURER_GPSR_ADDITIONAL_3' => '',
+];
+
+return $define;
