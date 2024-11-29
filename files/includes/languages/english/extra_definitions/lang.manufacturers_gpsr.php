5085360f63a84f0752a6a61739b5f8276a1c1cbe
torvista <4391026+torvista@users.noreply.github.com>
2024-11-29 10:39:53 +0100

    template: move to module, rework layout, add css

--- files/includes/languages/english/extra_definitions/lang.manufacturers_gpsr.php
+++ files/includes/languages/english/extra_definitions/lang.manufacturers_gpsr.php
@@ -1,5 +1,7 @@
 <?php
-/** 
+
+declare(strict_types=1);
+/**
  * Plugin GPSR
  * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
@@ -9,11 +11,11 @@
  */
 
 $define = [
-    'TEXT_MANUFACTURER_GPSR_INFO' => 'Manufacturer Info ',
-    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO' => 'Contact information in accordance with Art. 19 EU GPSR',
-    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO_NONEU' => 'Person responsible in accordance with Art. 19 EU GPSR',
-    'TEXT_MANUFACTURER_GPSR_ADDRESS_INFO' => 'Post Address:',
-    'TEXT_MANUFACTURER_GPSR_URL_INFO' => 'Electronical Address:',
+    'TEXT_MANUFACTURER_GPSR_INFO' => 'Manufacturer Contact information<br><span class="smallText">(in accordance with Art. 19 EU GPSR)</span>',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO' => 'Contact Details',
+    'TEXT_MANUFACTURER_GPSR_CONTACT_INFO_NONEU' => 'EU Contact Details',
+    'TEXT_MANUFACTURER_GPSR_ADDRESS_INFO' => 'Postal Address:',
+    //'TEXT_MANUFACTURER_GPSR_URL_INFO' => 'Electronic Address:',//todo remove
     'TEXT_MANUFACTURER_GPSR_COMPANY' => '',
     'TEXT_MANUFACTURER_GPSR_CONTACT_PERSON' => '',
     'TEXT_MANUFACTURER_GPSR_STREET' => '',
@@ -27,7 +29,7 @@ $define = [
     'TEXT_MANUFACTURER_GPSR_POSTCODE_NONEU' => '',
     'TEXT_MANUFACTURER_GPSR_CITY_NONEU' => '',
     'TEXT_MANUFACTURER_GPSR_COUNTRY_NONEU' => '',
-    'TEXT_MANUFACTURER_GPSR_URL_NONEU' => 'Website: ',
+    'TEXT_MANUFACTURER_GPSR_URL_NONEU' => 'Website:',
     'TEXT_MANUFACTURER_GPSR_ADDITIONAL_1' => '',
     'TEXT_MANUFACTURER_GPSR_ADDITIONAL_2' => '',
     'TEXT_MANUFACTURER_GPSR_ADDITIONAL_3' => '',
